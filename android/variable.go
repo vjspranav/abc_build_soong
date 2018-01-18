3079be880fe622cb50de395eb15a26365b20ce24
vjspranav <pranavasri@live.in>
2018-01-18 18:46:50 +0000

    Add soong-products

--- android/variable.go
+++ android/variable.go
@@ -67,6 +67,15 @@ type variableProperties struct {
 			Cflags []string
 		}
 
+
+		Uses_non_treble_camera struct {
+		Cflags []string
+		}
+
+        	Uses_media_extensions struct {
+		Cflags []string
+		}
+
 		// treble is true when a build is a Treble compliant device.  This is automatically set when
 		// a build is shipped with Android O, but can be overriden.  This controls such things as
 		// the sepolicy split and enabling the Treble linker namespaces.
@@ -140,6 +149,9 @@ type productVariables struct {
 	Device_uses_hwc2           *bool `json:",omitempty"`
 	Treble                     *bool `json:",omitempty"`
 	Pdk                        *bool `json:",omitempty"`
+
+       	Uses_media_extensions      *bool `json:",omitempty"`
+	Uses_non_treble_camera     *bool `json:",omitempty"`
 
 	IntegerOverflowExcludePaths *[]string `json:",omitempty"`
 
